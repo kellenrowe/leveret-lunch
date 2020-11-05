2c7bc0dffc49027e178e5e9a0a893a59803ee576
kellenrowe <kellenrowe@gmail.com>
2020-11-05 15:01:36 -0800

    added all files with function for finding center

---
+++ lunch.js
@@ -0,0 +1,28 @@
+/** lunchCount: return # of carrots eaten
+ *
+ * params:
+ * - garden: matrix of carrot counts
+ *
+ * returns: # of carrots
+ */
+
+function lunchCount(garden) {
+    // create count for carrots
+    let totalCarrots = 0;
+    // determine where the middle/start point is (other function)
+
+    // change eaten patch to 0
+    // return count for carrots
+    return totalCarrots;
+}
+
+function findCenter(garden) {
+    if (garden.length % 2 === 1) {
+        // find middle row
+        let row = Math.floor(garden.length / 2);
+    } 
+}
+
+// if garden.length % 2 == 1 => math.floor to determine row
+// if garden[i].length % 2 == "" ""
+// if garden is even 
